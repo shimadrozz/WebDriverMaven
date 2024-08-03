443ba0d7cd84068dde06a1d116c7a48d7d77a346
neatworks <neatworksvenue@gmail.com>
2024-08-03 13:40:52 -0400

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,36 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("shimadrozz@gmail.com");
+		//driver.findElement(By.id("Passwrd")).sendKeys("sfsfsfsf");
+		
+	}
+	
+	@AfterSuite
+	public void TearDown() {
+		
+		driver.quit();
+
+	}
+
+}
